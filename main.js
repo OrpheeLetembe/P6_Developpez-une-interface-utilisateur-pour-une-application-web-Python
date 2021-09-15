703f189a78a65761138b490afd3959e2af96d032
Orphée <letembeorphee@gmail.com>
2021-09-15 22:15:01 +0200

    add info best movie

--- main.js
+++ main.js
@@ -15,3 +15,29 @@ function httpRequest(id, traitement){
 	xhr.responseType = "json";
 	xhr.send();
 }
+
+// BEST MOVIE
+
+const bestMovie = document.getElementById("best-movie");
+const bestMovieTitle = document.getElementById("best-movie__title");
+const bestMovieImg = document.getElementById("best-movie__img");
+const bestMovieAbstract = document.getElementById("best-movie__abstract");
+
+let bestMovieScore = "?imdb_score_min=9.6"
+
+function getbestMovieAbstract(data){
+	bestMovieAbstract.innerHTML = data.description;
+}
+
+
+function getBestMovieData(data){
+	let bestMovieData = data.results;
+	bestMovieTitle.innerHTML = bestMovieData[0].title;
+	bestMovieImg.setAttribute("src", bestMovieData[0].image_url);
+	let bestMovieId = bestMovieData[0].id;
+
+	let bestMovieDetail = httpRequest(bestMovieId, getbestMovieAbstract);
+}
+
+
+let bestMovieInfos = httpRequest(bestMovieScore, getBestMovieData)
